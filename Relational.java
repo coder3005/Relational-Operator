74af0a91453ee4a4d6b1d553b0225881d79c3416
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-16 20:52:10 +0530

    Create Relational.java

---
+++ Relational.java
@@ -0,0 +1,18 @@
+/* Relational Operator Example...! */
+import java.util.Scanner;
+class Relational
+{
+    public static void main(String[] args) {
+        int a,b;
+        System.out.println("Enter two Number.. ");
+        Scanner obj=new Scanner(System.in);
+        a=obj.nextInt();
+        b=obj.nextInt();
+        System.out.println("true/false "+(a<b));
+        System.out.println("true/false "+(a>b));
+        System.out.println("true/false "+(a<=b));
+        System.out.println("true/false "+(a>=b));
+        System.out.println("true/false "+(a==b));
+        System.out.println("true/false "+(a!=b));
+    }
+}
